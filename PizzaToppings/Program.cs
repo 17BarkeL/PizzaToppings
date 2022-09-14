7ad8d7652551867513f88f1277ef63b3403115c3
Luke Barkess <luke.barkess@icloud.com>
2022-09-14 11:49:32 +0100

    dun?

--- PizzaToppings/Program.cs
+++ PizzaToppings/Program.cs
@@ -10,6 +10,21 @@ namespace PizzaToppings
     {
         static void Main(string[] args)
         {
+            List<string> nonVegetarianPizzaToppings = new List<string> { "CHICKEN", "HAM", "MEAT", "PEPPERONI", "SALAMI" };
+            Console.WriteLine("What pizza topping do you want?");
+            string pizzaToppingInput = Console.ReadLine().ToUpper();
+
+            if (nonVegetarianPizzaToppings.Contains(pizzaToppingInput))
+            {
+                Console.WriteLine("Not Vegetarian Topping");
+            }
+
+            else
+            {
+                Console.WriteLine("Vegetarian Topping");
+            }
+
+            Console.ReadLine();
         }
     }
 }
